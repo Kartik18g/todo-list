2620e62d7a96014b28a74a68fbd55e86f74598b5
Kartik Gupta <gupta.kartik18kg@gmail.com>
2021-08-20 11:57:16 +0530

    drag annd routes pending

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const list = document.querySelector('ol.todo-list')
+const list = document.querySelector('ol.todo-list div.items')
 
 // event listeners
 document.querySelector('.fas.fa-plus').addEventListener('click', () => {
@@ -6,10 +6,12 @@ document.querySelector('.fas.fa-plus').addEventListener('click', () => {
     addChore(input)
 })
 
-
-const handleClick = id => {
-    console.log(id)
+const clearList = () => {
+    list.innerHTML = null
 }
+
+
+
 
 const storage = (action, item, data = []) => {
     if (item.length > 0) switch (action) {
@@ -25,57 +27,82 @@ const storage = (action, item, data = []) => {
     }
 }
 
+const renderList = (chores) => {
+
+    // set total number
+    document.querySelector('.taskbar div:nth-child(1)').innerHTML = `${chores.length} items`
+
+    chores.length > 0 && chores.forEach((chore, iteration) => {
+        var node = document.createElement('div');
+        node.setAttribute('class', 'list-item')
+        node.setAttribute('id', `item-${iteration + 1}`)
+        node.innerHTML = createListItem(chore, iteration + 1)
+        list.appendChild(node);
+        // cross event listener
+        document.querySelector(`i.cross-${iteration + 1}`).addEventListener('click', () => {
+            removeChore(chore.name, `item-${iteration + 1}`)
+        })
+        // check event listener
+        document.querySelector(`#check-${iteration + 1}`).addEventListener('click', () => {
+            handleCheck(chore.name, `check-${iteration + 1}`)
+        })
+    })
+}
 
 
-const createListItem = (str, id) => {
+const handleCheck = (chore, id) => {
+    const item = chores.find(item => item.name === chore)
+    const index = chores.indexOf(item)
+    chores[index] = { name: item.name, status: item.status === 'pending' ? 'complete' : 'pending' }
+    // save to storage
+    storage('set', 'chores', chores)
+
+    clearList()
+    renderList(chores)
+}
+
+
+const createListItem = (chore, id) => {
     return `
     <section title=".roundedTwo">
         <div class="roundedTwo">
-            <input type="checkbox" value="None" name=check-${id}  />
+            <input id=check-${id} type="checkbox" ${chore.status === 'complete' ? 'checked' : ''} value="None" name=check-${id}  />
             <label class="status" for=check-${id}></label>
         </div>
     </section>
-    <div class="list-input" >${str}</div>
+    <div  class="list-input ${chore.status === 'complete' ? 'strike' : ''} " >${chore.name}</div>
     <i class="fas fa-times cross-${id}"></i>
     <i class="fas fa-grip-lines"></i>
 `
 }
 
-const removeChore = (id) => {
-    console.log(id)
+const removeChore = (chore, id) => {
     const itemToRemove = document.querySelector(`#${id}`)
-    console.log(itemToRemove)
     itemToRemove.classList.add("remove")
 
     setTimeout(() => {
-        // remove from array & update localStorage
-        chores.splice(id.split('-')[1] - 1, 1)
+        // remove from array 
+        chores.splice(chores.indexOf(chores.find(item => item.name === chore)), 1)
+        // & update localStorage
         storage('set', 'chores', chores)
         // update the markup
         list.removeChild(itemToRemove)
-    }, 1000)
+        // set total number
+        document.querySelector('.taskbar div:nth-child(1)').innerHTML = `${chores.length} items`
+    }, 500)
+
 
 }
 
 const addChore = (input) => {
     const { value: chore } = input
     const handleValidChore = (chore) => {
-        chores.push(chore)
+        chores.push({ name: chore, status: "pending" })
         storage('set', "chores", chores)
-        var node = document.createElement('div');
-        node.setAttribute('class', 'list-item')
-        node.setAttribute('id', `item-${chores.length}`)
-        node.innerHTML = createListItem(chore, chores.length)
-        list.appendChild(node);
-        const index = chores.length
-        // add cross event listener
-        document.querySelector(`i.cross-${chores.length}`).addEventListener('click', () => {
-            removeChore(`item-${index}`)
-        }
-        )
+        clearList()
+        renderList(chores)
     }
-    console.log(chores)
-    chore.length === 0 ? alert("Enter something") : chores.includes(chore) ? alert("Chore already exists") : handleValidChore(chore)
+    chore.length === 0 ? alert("Enter something") : chores.find(item => item.name == chore) ? alert("Chore already exists") : handleValidChore(chore)
 }
 
 
@@ -83,18 +110,6 @@ const addChore = (input) => {
 let chores = storage('get', 'chores') ? storage('get', 'chores') : []
 console.log(chores)
 
-chores.length > 0 && chores.forEach((chore, iteration) => {
-    var node = document.createElement('div');
-    node.setAttribute('class', 'list-item')
-    node.setAttribute('id', `item-${iteration + 1}`)
-    node.innerHTML = createListItem(chore, iteration + 1)
-    list.appendChild(node);
-    document.querySelector(`i.cross-${iteration + 1}`).addEventListener('click', () => {
-        removeChore(`item-${iteration + 1}`)
-    })
-})
 
-
-
-
+renderList(chores)
 
